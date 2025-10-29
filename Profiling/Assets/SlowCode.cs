40012ac7394129f6ab0ee13b83cff54c2bbb5c9a
hammone2 <hammone2@my.erau.edu>
2025-10-29 11:46:09 -0700

    scene setup

---
+++ Profiling/Assets/SlowCode.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SlowCode : MonoBehaviour
+{
+    private float timeValueRed;
+    private float timeValueGreen;
+    private float timeValueBlue;
+    void Update()
+    {
+        GetComponent<Renderer>().material.color = ColorForTime();
+        DoLoopStuff();
+        DoSomethingElse();
+    }
+    private void DoLoopStuff()
+    {
+        for (int i = 0; i < 2500; i++)
+        {
+            GetComponent<Renderer>().material.color = ColorForTime();
+        }
+    }
+    private Color ColorForTime()
+    {
+        timeValueRed += Time.deltaTime;
+        if (timeValueRed > 1f)
+            timeValueRed -= 1f;
+        timeValueGreen += Time.deltaTime * 2f;
+        if (timeValueGreen > 1f)
+            timeValueGreen -= 1f;
+        timeValueBlue += Time.deltaTime * 3f;
+        if (timeValueBlue > 1f)
+            timeValueBlue -= 1f;
+        return new Color(timeValueRed, timeValueGreen, timeValueBlue);
+    }
+    private void DoSomethingElse()
+    {
+        Debug.Log("look how slow logging is!");
+    }
+}
